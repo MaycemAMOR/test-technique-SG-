d7d3467cc2c302f3ab2eced5870f296a9cd8eab9
Maycem AMOR <maycemamor@gmail.com>
2024-06-28 01:39:03 +0200

    -Enhance the Service.java code by making the calculation faster: The tax income calculation per range is done in advance and stored in a map.

--- something/src/main/java/training/lab/Service.java
+++ something/src/main/java/training/lab/Service.java
@@ -1,14 +1,17 @@
 package training.lab;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static java.math.RoundingMode.CEILING;
 import static training.lab.IndividualType.ADULT;
 import static training.lab.IndividualType.CHILD;
 
 public class Service {
+
 
     private static final BigDecimal HALF = BigDecimal.valueOf(0.5);
     private static final BigDecimal ZERO = BigDecimal.ZERO;
@@ -29,6 +32,33 @@ public class Service {
             BigDecimal.valueOf(41),
             BigDecimal.valueOf(45)
     );
+    private static final Map<Integer, BigDecimal> computeTaxIncomePerRangeMap = computeTaxIncomePerRange();
+
+    static {
+        System.out.println("Tax Income Value Per Range: ");
+        for (Map.Entry<Integer, BigDecimal> entry : computeTaxIncomePerRangeMap.entrySet()) {
+            Integer key = entry.getKey();
+            BigDecimal value = entry.getValue();
+            System.out.println("Range: " + key + ", Value: " + value);
+        }
+        System.out.println("\n");
+    }
+
+
+    private static Map<Integer, BigDecimal> computeTaxIncomePerRange() {
+        Map<Integer, BigDecimal> taxIncomePerRangeMap = new HashMap<>();
+        taxIncomePerRangeMap.put(0, ZERO);
+        for (int i = 0; i < BASIS_INCOME_SCALE_LIST.size(); i++) {
+            if (i + 1 < BASIS_INCOME_SCALE_LIST.size()) {
+                taxIncomePerRangeMap.put(i + 1,
+                        BASIS_INCOME_SCALE_LIST.get(i + 1)
+                                .subtract(BASIS_INCOME_SCALE_LIST.get(i))
+                                .multiply(GROSS_TAX_LIST.get(i))
+                                .divide(HUNDRED, SCALE, CEILING));
+            }
+        }
+        return taxIncomePerRangeMap;
+    }
 
     private static int computeGrossTaxIncomeRange(BigDecimal basis) {
         int grossTaxIncomeRange = 0;
@@ -42,28 +72,20 @@ public class Service {
 
     private static BigDecimal computeTaxIncome(int grossTaxIncomeRange, BigDecimal basis) {
         BigDecimal taxIncome = ZERO;
-        if (grossTaxIncomeRange > 0) {
-            for (int i = 0; i <= grossTaxIncomeRange; i++) {
-                if (i != grossTaxIncomeRange) {
-                    taxIncome = taxIncome
-                            .add(((BASIS_INCOME_SCALE_LIST.get(i + 1)
-                                    .subtract(BASIS_INCOME_SCALE_LIST.get(i)))
-                                    .multiply(GROSS_TAX_LIST.get(i)))
-                                    .divide(HUNDRED, SCALE, RoundingMode.CEILING));
-                }
-                if (i == grossTaxIncomeRange) {
-                    taxIncome = taxIncome
-                            .add(((basis.subtract(BASIS_INCOME_SCALE_LIST.get(i)))
-                                    .multiply(GROSS_TAX_LIST.get(i)))
-                                    .divide(HUNDRED, SCALE, RoundingMode.CEILING));
-                }
+        int i = 0;
+        for (Map.Entry<Integer, BigDecimal> entry : Service.computeTaxIncomePerRangeMap.entrySet()) {
+            if (i <= grossTaxIncomeRange) {
+                BigDecimal value = entry.getValue();
+                taxIncome = taxIncome.add(value);
+                i++;
+            } else {
+                break;
             }
-        } else {
-            taxIncome = taxIncome
-                    .add(((basis.subtract(BASIS_INCOME_SCALE_LIST.get(0)))
-                            .multiply(GROSS_TAX_LIST.get(0)))
-                            .divide(HUNDRED, SCALE, RoundingMode.CEILING));
         }
+        taxIncome = taxIncome
+                .add(((basis.subtract(BASIS_INCOME_SCALE_LIST.get(grossTaxIncomeRange)))
+                        .multiply(GROSS_TAX_LIST.get(grossTaxIncomeRange)))
+                        .divide(HUNDRED, SCALE, CEILING));
         return taxIncome;
     }
 
@@ -84,15 +106,15 @@ public class Service {
         BigDecimal familyQuotient = BigDecimal.valueOf(adult).add(BigDecimal.valueOf(child).multiply(HALF));
         System.out.println("familyQuotient : " + familyQuotient);
         System.out.println("totalRevenues : " + totalRevenues);
-        BigDecimal basis = totalRevenues.divide(familyQuotient, SCALE, RoundingMode.CEILING);
+        BigDecimal basis = totalRevenues.divide(familyQuotient, SCALE, CEILING);
 
         System.out.println("basis : " + basis);
-        System.out.println("GrossTaxIncomeRange : " + (computeGrossTaxIncomeRange(basis) + 2) + " Range");
+        System.out.println("GrossTaxIncomeRange : " + (computeGrossTaxIncomeRange(basis)) + " Range");
 
         // compute the taxIncome
         BigDecimal taxIncome = computeTaxIncome(computeGrossTaxIncomeRange(basis), basis);
 
-        System.out.println("taxIncome : " + taxIncome);
+        System.out.println("taxIncome : " + taxIncome + "\n");
 
         return taxIncome;
     }
